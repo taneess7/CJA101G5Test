ba786b791079443af05fe6b0a5b2d0c1b054b043
chan <h1257465876@gmail.com>
2025-05-16 22:26:20 +0800

    新增postvo和MESSAGEVO

--- src/main/java/com/foodtimetest/post/PostVO.java
+++ src/main/java/com/foodtimetest/post/PostVO.java
@@ -1,11 +1,19 @@
 package com.foodtimetest.post;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class PostVO implements Serializable{
 	private int PostId;
 	private int MemId;
+	private Date PostDate;
 	
+	public Date getPostDate() {
+		return PostDate;
+	}
+	public void setPostDate(Date postDate) {
+		PostDate = postDate;
+	}
 	public int getPostId() {
 		return PostId;
 	}
