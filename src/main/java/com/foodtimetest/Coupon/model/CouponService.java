0677566c0ab7236d5083e3cc4f0466feae52c701
Jamie Wu <meigo1229@gmail.com>
2025-05-20 20:35:22 +0800

    feat: add CouponService

---
+++ src/main/java/com/foodtimetest/Coupon/model/CouponService.java
@@ -0,0 +1,54 @@
+package com.foodtimetest.Coupon.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public class CouponService {
+
+	private CouponDAO_interface dao;
+
+	public CouponService() {
+		dao = new CouponDAO();
+	}
+
+	public CouponVO addCoupon(Integer storId, String couDes, String couType, Integer couMinOrd, Timestamp couDate) {
+
+		CouponVO couponVO = new CouponVO();
+
+		couponVO.setStorId(storId);
+		couponVO.setCouDes(couDes);
+		couponVO.setCouType(couType);
+		couponVO.setCouMinOrd(couMinOrd);
+		couponVO.setCouDate(couDate);
+
+		return couponVO;
+	}
+
+	public CouponVO updateCoupon(Integer couId, Integer storId, String couDes, String couType, Integer couMinOrd,
+			Timestamp couDate) {
+
+		CouponVO couponVO = new CouponVO();
+
+		couponVO.setCouId(couId);
+		couponVO.setStorId(storId);
+		couponVO.setCouDes(couDes);
+		couponVO.setCouType(couType);
+		couponVO.setCouMinOrd(couMinOrd);
+		couponVO.setCouDate(couDate);
+
+		return couponVO;
+	}
+
+	public void deleteCoupon(Integer couId) {
+		dao.delete(couId);
+	}
+
+	public CouponVO getOneCoupon(Integer couId) {
+		return dao.findByPrimaryKey(couId);
+	}
+
+	public List<CouponVO> getAll() {
+		return dao.getAll();
+	}
+
+}
