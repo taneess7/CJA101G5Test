218ad18645572432b8bb06ffc388529ac307b6e5
Max Sun <max8902181@hotmail.com>
2025-05-23 19:46:53 +0800

    FEAT:新增功能
    新增帳號是否存在方法

--- src/main/java/com/foodtimetest/member/MemDAO_interface.java
+++ src/main/java/com/foodtimetest/member/MemDAO_interface.java
@@ -9,4 +9,5 @@ public interface MemDAO_interface {
 	public void delete(Integer memId);//刪除會員資料
 	public MemberVO findByPrimaryKey(Integer memId);//尋找某一個會員資料
 	public List<MemberVO> getAll();//將會員資料放在陣列
+	public boolean isAccountExist(String memAccount);
 }
