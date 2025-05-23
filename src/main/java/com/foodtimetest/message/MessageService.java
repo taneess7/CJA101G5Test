cd4b09af1a78610674f82df83ba7dd57fa64d998
chan <h1257465876@gmail.com>
2025-05-23 21:22:24 +0800

    Feat :新增messageService

--- src/main/java/com/foodtimetest/message/MessageService.java
+++ src/main/java/com/foodtimetest/message/MessageService.java
@@ -8,6 +8,20 @@ public class MessageService {
 	public MessageService() {
 			dao = new MessageDAO();
 		}
+	public MessageVO addMessage(Integer postId, Integer memId, java.sql.Timestamp mesDate, String mesContent) {
+		MessageVO messageVO = new MessageVO();
+		messageVO.setPostId(postId);
+		messageVO.setMemId(memId);
+		messageVO.setMesDate(mesDate);
+		messageVO.setMesContent(mesContent);
+		dao.insert(messageVO);
+		return messageVO;
+	}
 	
+//	public MessageVO updateMessage(Integer mesId) {
+//		MessageVO messageVO = new MessageVO();
+//		messageVO.setMemId(mesId);
+//		messageVO.setMesContent(null);
+//	}
 
 }
